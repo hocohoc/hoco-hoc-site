4a04f024f6fcd4c518966d308ebff5ac7e323630
Ankit Mohanty <ankitvmohanty5@gmail.com>
2025-11-05 19:05:11 -0500

    fixed

--- src/app/components/team/teamCard.tsx
+++ src/app/components/team/teamCard.tsx
@@ -10,7 +10,7 @@ type Member = {
 
 export default function TeamCard(props: { member: Member }) {
     return (
-        <div className="flex flex-col items-center gap-2">
+        <div className="bg-slate-800 border-2 border-slate-700 rounded-lg p-4 flex flex-col items-center gap-3">
             <div className="rounded-full overflow-hidden">
                 <Image src={props.member.image} alt={`${props.member.name}, ${props.member.position}`} width={80} height={80} className="w-20 h-20 object-cover" />
             </div>
