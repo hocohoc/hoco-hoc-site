a22416413272dcf0974f29fd464718d1b4ebfd7e
Ankit Mohanty <ankitvmohanty5@gmail.com>
2025-11-05 19:07:07 -0500

    team

--- src/app/components/team/teamCard.tsx
+++ src/app/components/team/teamCard.tsx
@@ -11,8 +11,8 @@ type Member = {
 export default function TeamCard(props: { member: Member }) {
     return (
         <div className="bg-slate-800 border-2 border-slate-700 rounded-lg p-4 flex flex-col items-center gap-3">
-            <div className="rounded-full overflow-hidden">
-                <Image src={props.member.image} alt={`${props.member.name}, ${props.member.position}`} width={80} height={80} className="w-20 h-20 object-cover" />
+            <div className="overflow-hidden rounded-lg">
+                <Image src={props.member.image} alt={`${props.member.name}, ${props.member.position}`} width={120} height={120} className="w-32 h-32 object-contain" />
             </div>
             <div className="text-center">
                 <p className="text-md font-bold">{props.member.name}</p>
